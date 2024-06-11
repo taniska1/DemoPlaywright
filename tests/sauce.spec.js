883ee435ef1153451cd9a644ee91a991b9e8ff7c
Btouili <touilibilel@yahoo.fr>
2024-06-11 16:01:47 +0100

    [BUG] bug.

--- tests/sauce.spec.js
+++ tests/sauce.spec.js
@@ -13,6 +13,6 @@ test("user should not be able to login", async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await Auth.login(page, 'locked_out_user', 'secret_sauce')
     await expect(page.locator('[data-test="error"]')).toBeVisible();
-    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
+    await expect(page.locator('[data-test="error"]')).toContainText('sadface: Sorry, this user has been locked out.');
 });
 
